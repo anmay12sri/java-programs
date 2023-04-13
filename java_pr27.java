48a7d7cc2f830d3a830ccb92321836b3f25ff18a
anmay12sri <103562464+anmay12sri@users.noreply.github.com>
2023-04-13 23:10:56 +0530

    Create java_pr27.java

---
+++ java_pr27.java
@@ -0,0 +1,15 @@
+/*normal addition using method*/
+
+
+
+public class addmethod {
+    public static void main(String args[]){
+        int x=3;
+        int y=8;
+        System.out.println(add(x,y));
+       // System.out.print(sub(x,y));
+    }
+    static int add(int x,int y){
+        return x+y;
+    }
+}
